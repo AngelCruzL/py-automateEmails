efd254c3ec55c95de5f4da8ec03218ae7b62d593
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2021-12-13 13:25:33 -0600

    Adding script to sent an email with attachment
    - Modify csv file to add new data rows
    - Adding bill 1, 2 and 3 to attach it

---
+++ 06-send-email-with-attachment-to-contacts.py
@@ -0,0 +1,21 @@
+import yagmail
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+sender = 'langelcruzlara@gmail.com'
+subject = 'This is an email test with an attachment'
+yag = yagmail.SMTP(user=sender, password=os.environ['PASSWORD'])
+
+df = pd.read_csv('contacts.csv')
+
+for index, row in df.iterrows():
+    contents = [f"""
+Hey {row['name']}, you have to pay {row['amount']}.
+bill is attached.
+""", row['filepath']]
+
+    yag.send(to=row['email'], subject=subject, contents=contents)
+    print("Email sent! 🚀")
