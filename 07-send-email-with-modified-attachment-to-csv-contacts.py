42cc9879b877f4509923fc43cc3187ab84ebb1f8
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2021-12-13 15:48:23 -0600

    Adding an email with modified attachment

---
+++ 07-send-email-with-modified-attachment-to-csv-contacts.py
@@ -0,0 +1,34 @@
+import yagmail
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+sender = 'langelcruzlara@gmail.com'
+yag = yagmail.SMTP(user=sender, password=os.environ['PASSWORD'])
+
+df = pd.read_csv('contacts.csv')
+
+
+def generate_filename(filename, content):
+    with open(filename, 'w') as file:
+        file.write(str(content))
+
+
+for index, row in df.iterrows():
+    name = row['name']
+    filename = f'{name}.txt'
+    amount = row['amount']
+    receiver_email = row['email']
+
+    generate_filename(filename, amount)
+
+    subject = 'This is an email test with an modified attachment!'
+    contents = [f"""
+Hey {name}, you have to pay {amount}.
+Bill is attached!.""",
+                filename]
+
+    yag.send(to=row['email'], subject=subject, contents=contents)
+    print("Email sent! 🚀")
